372181035aaaf9b9c3170ca1201e541319997fa1
Aleksei <jermlay2010@gmail.com>
2024-07-07 16:43:05 +0300

    refactoring

--- src/app/scripts/burger-menu.js
+++ src/app/scripts/burger-menu.js
@@ -2,59 +2,37 @@ const body = document.body,
       openBurgerBtn = document.querySelector('#open-burger-btn'),
       closeBurgerBtn = document.querySelector('#close-burger-btn'),
       menuList = document.querySelector('#menu-list'),
-      menuLinks = document.querySelectorAll('.menu__list_item a');
+      menuLinks = document.querySelectorAll('.menu-mobile__list_item a');
         
 
-function enableScroll() {
+const enableScroll = () => {
     body.classList.remove('no-scroll');
 };
-function disableScroll() {
+const disableScroll = () => {
     body.classList.add('no-scroll');
 };
 
-function runForSmallScreens() {
-    menuList.style.visibility = 'hidden';
-    menuLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            menuList.style.visibility = 'hidden';
-            closeBurgerBtn.classList.add('display-none');
-            openBurgerBtn.classList.remove('display-none');
-            enableScroll();
-        });
-    });
-    
-    openBurgerBtn.addEventListener('click', () => {
-        openBurgerBtn.classList.add('display-none');
-        closeBurgerBtn.classList.remove('display-none');
-        menuList.style.visibility = 'visible';
-        disableScroll();
-    });
-    
-    
-    closeBurgerBtn.addEventListener('click', () => {
-        openBurgerBtn.classList.remove('display-none');
-        closeBurgerBtn.classList.add('display-none');
+menuList.style.visibility = 'hidden';
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
         menuList.style.visibility = 'hidden';
+        closeBurgerBtn.classList.add('display-none');
+        openBurgerBtn.classList.remove('display-none');
         enableScroll();
     });
-}
+});
 
-function runForLargeScreens() {
+openBurgerBtn.addEventListener('click', () => {
+    openBurgerBtn.classList.add('display-none');
+    closeBurgerBtn.classList.remove('display-none');
     menuList.style.visibility = 'visible';
-    menuLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            menuList.style.visibility = 'visible';
-        });
-    });
-}
+    disableScroll();
+});
 
-function checkScreenSize() {
-    if (window.innerWidth >= 1025) {
-        runForLargeScreens();
-    } else {
-        runForSmallScreens();
-    }
-}
 
-checkScreenSize();
-window.addEventListener('resize', checkScreenSize);
+closeBurgerBtn.addEventListener('click', () => {
+    openBurgerBtn.classList.remove('display-none');
+    closeBurgerBtn.classList.add('display-none');
+    menuList.style.visibility = 'hidden';
+    enableScroll();
+});
