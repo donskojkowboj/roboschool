ef5b0a089558e28b3f1261504914afd21e16b71a
Aleksei <jermlay2010@gmail.com>
2024-07-11 00:12:24 +0300

    воспроизведение кода со значительными  подглядываниями

--- src/app/scripts/slider.js
+++ src/app/scripts/slider.js
@@ -1,76 +1,49 @@
 const slider = document.querySelector('#slider'),
-      slides = document.querySelectorAll('.trainers__list_item'),
-      btnPrev = document.querySelector('#btn-prev'),
-      btnNext = document.querySelector('#btn-next');
+      btns = document.querySelectorAll('.trainer__buttons_arrows'),
+      track = document.querySelector('#scrollbar-track'),
+      thumb = document.querySelector('#scrollbar-thumb');
 
+let maxScrollLeft = slider.scrollWidth - slider.clientWidth;
 
-const sliderWidth = slider.clientWidth,
-      slidesGap = 40,
-      slideWidth = slides[0].offsetWidth,
-      totalSlideWidth = slideWidth + slidesGap,
-      totalSlidesNumber = slider.children.length;
-
-const track = document.querySelector('#scrollbar-track'),
-      thumb = document.querySelector('#scrollbar-thumb'),
-      trackRect = track.getBoundingClientRect(),
-      thumbRect = thumb.getBoundingClientRect();
-      
-
-let dragging = false;
-
-const moveStart = (e) => {
-    e.preventDefault();
-    dragging = true;
-    document.addEventListener('mousemove', moving);
-    document.addEventListener('mouseup', moveEnd);
-    
-}
-
-const moveEnd = (e) => {
-    e.preventDefault();
-    dragging = false;
-    document.removeEventListener('mousemove', moving);
-    document.removeEventListener('mouseup', moveEnd);
-}
-
-const moving = (e) => {
-    e.preventDefault();
-    if (dragging) {
-        let newLeft = e.clientX - trackRect.left - (thumbRect.width / 2);
-        if (newLeft < 0) {
-            newLeft = 0;
-        } 
-        if (newLeft > trackRect.width - thumbRect.width) {
-            newLeft = trackRect.width - thumbRect.width;
-        }
-        thumb.style.left = `${newLeft}px`;
-
-        const thumbPositionPercentage = newLeft / (trackRect.width - thumbRect.width);
-        const maxScrollLeft = slider.scrollWidth - slider.clientWidth; 
-
-        slider.scrollTo({
-            left: thumbPositionPercentage * maxScrollLeft
+btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const direction = btn.id === 'btn-prev' ? -1 : 1;
+        const scroll = direction * slider.clientWidth;
+        slider.scrollBy({
+            left: scroll,
+            behavior: 'smooth'
         });
-    }
-}
-
-thumb.addEventListener('mousedown', moveStart);
-
-
-let slideIndex = 0;
-
-btnPrev.addEventListener('click', () => {
-    slideIndex > 0 && slideIndex--;
-
-    slider.scrollTo({
-        left: slideIndex * totalSlideWidth
     });
 });
 
-btnNext.addEventListener('click', () => {
-    slideIndex < totalSlidesNumber - 3 && slideIndex++;
+slider.addEventListener('scroll', () => {
+    const thumbPosition = (slider.scrollLeft / maxScrollLeft) * (track.clientWidth - thumb.offsetWidth);
+    thumb.style.left = `${thumbPosition}px`;
+});
 
-    slider.scrollTo({
-        left: slideIndex * totalSlideWidth
-    });
+thumb.addEventListener('mousedown', (e) => {
+    const startX = e.clientX;
+    const thumbPosition = thumb.offsetLeft;
+    const maxThumbPosition = track.getBoundingClientRect().width - thumb.offsetWidth;
+
+    const handleMouseMove = (e) => {
+        const deltaX = e.clientX - startX;
+        const newThumbPosition = thumbPosition + deltaX;
+        const boundedPosition = Math.max(0, Math.min(maxThumbPosition, newThumbPosition));
+        const scrollPosition = (boundedPosition / maxThumbPosition) * maxScrollLeft;
+        thumb.style.left = `${boundedPosition}px`;
+        slider.scrollLeft = scrollPosition;
+    };
+
+    const handleMouseUp = () => {
+        document.removeEventListener('mousemove', handleMouseMove);
+        document.removeEventListener('mouseup', handleMouseUp);
+    };
+
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', handleMouseUp);
+});
+
+window.addEventListener('resize', () => {
+    maxScrollLeft = slider.scrollWidth - slider.clientWidth;
 });
